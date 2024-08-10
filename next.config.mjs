0c9e3f4c18f8246ca35a648cebb763f18cd7b6f0
Пользователь <lilu163@mail.ru>
2024-08-10 21:09:16 +0400

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
+  output: 'export',
+  basePath: '/words',
+  assetPrefix: '/words/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
