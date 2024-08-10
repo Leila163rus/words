af4af15ce67435ccd9792318e1e9f492504109cf
Пользователь <lilu163@mail.ru>
2024-08-10 21:46:38 +0400

    g

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
   output: 'export',
   basePath: '/words',
-  assetPrefix: '/words/',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
